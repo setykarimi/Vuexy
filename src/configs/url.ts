187bc2a599229beddb74b59cb9521fa3006e3af3
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-09-06 13:33:47 +0330

    💭feat: Create new file for configs (url)

---
+++ src/configs/url.ts
@@ -0,0 +1,3 @@
+export const FRONT2DB = process.env.NEXT_PUBLIC_APP_FRONT2DB;
+export const FRONT2MESSAGE = process.env.NEXT_PUBLIC_APP_FRONT2MESSAGE;
+export const AUTH_URL =  process.env.NEXT_PUBLIC_APP_AUTH_URL;
