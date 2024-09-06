3f45bf2d33ea099a0cf4244540e7aa30f27b8000
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-09-06 13:35:16 +0330

    💭feat: Edit loginParams

--- src/context/types.ts
+++ src/context/types.ts
@@ -1,7 +1,7 @@
 export type ErrCallbackType = (err: { [key: string]: string }) => void
 
 export type LoginParams = {
-  email: string
+  phone_number: string
   password: string
   rememberMe?: boolean
 }
