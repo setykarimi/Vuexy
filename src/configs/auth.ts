95f264554e58576f38b1b3def3d3ea0196dc6556
Setayesh <75073451+setykarimi@users.noreply.github.com>
2024-09-07 07:17:35 +0330

    💭feat: Chage url for login (OTO)

--- src/configs/auth.ts
+++ src/configs/auth.ts
@@ -1,6 +1,6 @@
 export default {
   meEndpoint: '/auth/me',
-  loginEndpoint: '/jwt/login',
+  loginEndpoint: 'https://ipa4.metakhodro.ir/v2/Auth/User/Login/Password',
   registerEndpoint: '/jwt/register',
   storageTokenKeyName: 'accessToken',
   onTokenExpiration: 'refreshToken' // logout | refreshToken
